99dc7281e01a502cdf3ec2c6d8c3c3a6da4afe21
James Heimberger <james.heimberger@gmail.com>
2019-02-08 16:44:21 -0500

    Fixed capitalization with db.Model

--- services/todolist/project/api/models.py
+++ services/todolist/project/api/models.py
@@ -1,12 +1,12 @@
 from project import db
 
 
-class List(db.model):
+class List(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
 
 
-class Item(db.model):
+class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     item = db.Column(db.String(128), unique=True, nullable=False)
     list_id = db.Column(db.Integer, db.ForeignKey('list.id'), nullable=False)
