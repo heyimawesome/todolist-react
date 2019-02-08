0126a53a59b5d70e4adac4e6475f5fc40eb97e85
james <james@netlag.xyz>
2019-02-08 13:56:09 -0500

    Added some code for test setup

--- services/todolist/project/config.py
+++ services/todolist/project/config.py
@@ -1,2 +1,12 @@
+import os
+
+
 class BaseConfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+class TestingConfig(BaseConfig):
+    Testing = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_TEST')
