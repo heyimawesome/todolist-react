e8bf42ee40eeabb93fd63c20e487411c2c107668
James Heimberger <james.heimberger@gmail.com>
2019-02-08 19:29:04 -0500

    Added some List route tests

--- services/todolist/project/tests/test_list.py
+++ services/todolist/project/tests/test_list.py
@@ -1,29 +1,79 @@
 import json
+import unittest
 
+from project import db
+from project.api.models import List
 from project.tests.base import BaseTestCase
+
+
+def add_list(title):
+    new_list = List(title=title)
+    db.session.add(new_list)
+    db.session.commit()
+    return new_list
 
 
 class TestListBlueprint(BaseTestCase):
     def test_get_all_lists(self):
-        pass
+        add_list('This is a test list')
+        add_list('This is also a test list')
+
+        response = self.client.get('/list/')
+
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['status'], 'success')
+        self.assertEqual(len(data['data']['lists']), 2)
+        self.assertIn(
+            'This is a test list',
+            data['data']['lists'][0]
+        )
+        self.assertIn(
+            'This is also a test list',
+            data['data']['lists'][1]
+        )
 
     def test_get_single_list(self):
-        pass
+        add_list('This is a test list')
+        response = self.client.get('/list/1')
+
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['status'], 'success')
+        self.assertEqual(data['data']['title'], 'This is a test list')
 
     def test_add_list(self):
         response = self.client.post(
             '/list/add',
             data=json.dumps({
-                'name': 'test-list'
+                'title': 'test-list'
             }),
             content_type='application/json'
         )
 
         data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 201)
         self.assertEqual(data['status'], 'success')
         self.assertEqual(data['message'], 'List added.')
         self.assertEqual(response.content_type, 'application/json')
-        self.assertEqual(response.status_code, 201)
 
     def test_remove_list(self):
-        pass
+        add_list('This is a test list')
+        test_list = add_list('This is also a test list')
+        response = self.client.get('/list/')
+        data = json.loads(response.data.decode())
+        self.assertEqual(len(data['data']['lists']), 2)
+
+        response = self.client.get(f'/list/remove/{test_list.id}')
+        data = json.loads(response.data.decode())
+        self.assertEqual(data['status'], 'success')
+        self.assertEqual(data['message'], 'List removed.')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/list/')
+        data = json.loads(response.data.decode())
+        self.assertEqual(len(data['data']['lists']), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
